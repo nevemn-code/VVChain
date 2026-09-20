47131c2a7b82292dbad9a6416c04099aa1eec04a
nevemn-code <nevemann@gmail.com>
2026-09-20 22:47:30 +0800

    Expose detailed four-band module controls in APVTS

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -16,6 +16,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout VVChainAudioProcessor::creat
     for (int i = 0; i < 4; ++i)
     {
         const juce::String n = juce::String(i + 1);
+
         p.push_back(std::make_unique<juce::AudioParameterFloat>(
             "EQ" + n + "_FREQ", "EQ " + n + " Freq",
             juce::NormalisableRange<float>(20.f, 20000.f, 0.01f, 0.25f),
@@ -37,37 +38,47 @@ juce::AudioProcessorValueTreeState::ParameterLayout VVChainAudioProcessor::creat
             id, name, juce::NormalisableRange<float>(lo, hi, 0.01f, skew), def));
     };
 
-    f("HF_CORNER", "HF Corner", 40.f, 120.f, 70.f);
+    f("EQ_COLOR", "Analog Color", 0.f, 100.f, 35.f);
+    f("HF_CORNER", "HF / HPF", 40.f, 120.f, 70.f);
 
-    f("OTT_DEPTH", "OTT Depth", 0.f, 100.f, 50.f);
+    for (int i = 0; i < 4; ++i)
+        f("OTT_B" + juce::String(i + 1), "OTT Band " + juce::String(i + 1), 0.f, 100.f, 50.f);
+
     f("OTT_MIX", "OTT Mix", 0.f, 100.f, 50.f);
-    f("OTT_THRESHOLD", "OTT Threshold", -60.f, 0.f, -16.f);
-    f("OTT_UP_RATIO", "OTT Up Ratio", 1.f, 8.f, 2.f);
-    f("OTT_DOWN_RATIO", "OTT Down Ratio", 1.f, 20.f, 4.f);
-    f("OTT_ATTACK", "OTT Attack", 0.1f, 30.f, 2.5f, 0.35f);
+    f("OTT_THRESHOLD", "OTT Threshold", -60.f, 0.f, -24.f);
+    f("OTT_UP_RATIO", "OTT Up Ratio", 1.f, 8.f, 4.f);
+    f("OTT_DOWN_RATIO", "OTT Down Ratio", 1.f, 80.f, 20.f);
+    f("OTT_ATTACK", "OTT Attack", 0.2f, 50.f, 2.5f, 0.35f);
     f("OTT_RELEASE", "OTT Release", 10.f, 500.f, 80.f, 0.35f);
-    f("OTT_X1", "OTT Low/Mid", 60.f, 900.f, 180.f, 0.35f);
-    f("OTT_X2", "OTT Mid/High", 600.f, 3500.f, 1400.f, 0.35f);
-    f("OTT_X3", "OTT High/Air", 2500.f, 12000.f, 6500.f, 0.35f);
+    f("OTT_X1", "OTT Xover 1", 60.f, 900.f, 88.f, 0.35f);
+    f("OTT_X2", "OTT Xover 2", 600.f, 5000.f, 2500.f, 0.35f);
+    f("OTT_X3", "OTT Xover 3", 2500.f, 12000.f, 8500.f, 0.35f);
+    f("OTT_INPUT", "OTT Input Gain", -12.f, 12.f, 5.2f);
     f("OTT_POST", "OTT Post Gain", -18.f, 18.f, 0.f);
 
-    f("ATYPE_AMOUNT", "A-Type Amount", 0.f, 100.f, 20.f);
-    f("ATYPE_DRIVE", "A-Type Drive", 0.f, 24.f, 6.f);
-    f("ATYPE_BIAS", "A-Type Bias", -100.f, 100.f, 0.f);
-    f("ATYPE_MIX", "A-Type Mix", 0.f, 100.f, 100.f);
-    f("ATYPE_TONE", "A-Type Tone", 0.f, 100.f, 70.f);
-    f("ATYPE_HPF", "A-Type HPF", 20.f, 1000.f, 60.f, 0.35f);
+    const float typeAmountDefaults[4] = { 0.f, 20.f, 70.f, 55.f };
+    const float typeGainDefaults[4] = { 0.f, 0.f, 1.f, 1.f };
 
-    f("DEESS_FREQ", "De-Esser Freq", 2000.f, 12000.f, 6500.f, 0.35f);
-    f("DEESS_Q", "De-Esser Q", 0.20f, 10.f, 1.2f, 0.35f);
-    f("DEESS_THRESHOLD", "De-Esser Threshold", -80.f, 0.f, -30.f);
-    f("DEESS_RANGE", "De-Esser Range", 0.f, 24.f, 8.f);
+    for (int i = 0; i < 4; ++i)
+    {
+        const juce::String n = juce::String(i + 1);
+        f("ATYPE_B" + n, "Type-A Band " + n, 0.f, 100.f, typeAmountDefaults[i]);
+        f("ATYPE_GAIN" + n, "Type-A Gain " + n, -6.f, 6.f, typeGainDefaults[i]);
+    }
+
+    f("ATYPE_ATTACK", "Type-A Attack", 1.f, 100.f, 10.f, 0.35f);
+    f("ATYPE_RELEASE", "Type-A Release", 20.f, 500.f, 120.f, 0.35f);
+    f("ATYPE_MIX", "Type-A Mix", 0.f, 100.f, 100.f);
+
+    f("DEESS_LOW", "De-Esser Low Xover", 2500.f, 9000.f, 4500.f, 0.35f);
+    f("DEESS_HIGH", "De-Esser High Xover", 6000.f, 15000.f, 10500.f, 0.35f);
+    f("DEESS_RANGE", "De-Esser Range", 0.f, 24.f, 10.f);
+    f("DEESS_STRENGTH", "De-Esser Strength", 0.f, 100.f, 75.f);
     f("DEESS_ATTACK", "De-Esser Attack", 0.1f, 20.f, 1.f, 0.35f);
     f("DEESS_RELEASE", "De-Esser Release", 10.f, 300.f, 80.f, 0.35f);
-
     p.push_back(std::make_unique<juce::AudioParameterBool>("DEESS_LISTEN", "De-Esser Listen", false));
 
-    f("DRY_WET", "Dry/Wet", 0.f, 100.f, 100.f);
+    f("DRY_WET", "Dry / Wet", 0.f, 100.f, 100.f);
     f("OUTPUT_LEVEL", "Output Level", -24.f, 12.f, 0.f);
 
     return { p.begin(), p.end() };
@@ -99,41 +110,46 @@ void VVChainAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce:
         p.freq[(size_t)i] = apvts.getRawParameterValue("EQ" + n + "_FREQ")->load();
         p.gain[(size_t)i] = apvts.getRawParameterValue("EQ" + n + "_GAIN")->load();
         p.q[(size_t)i] = apvts.getRawParameterValue("EQ" + n + "_Q")->load();
+
+        p.ottAmount[(size_t)i] = apvts.getRawParameterValue("OTT_B" + n)->load();
+        p.atypeAmount[(size_t)i] = apvts.getRawParameterValue("ATYPE_B" + n)->load();
+        p.atypeGainDb[(size_t)i] = apvts.getRawParameterValue("ATYPE_GAIN" + n)->load();
     }
 
-    auto v = [this](const juce::String& id) { return apvts.getRawParameterValue(id)->load(); };
+    auto value = [this](const juce::String& id)
+    {
+        return apvts.getRawParameterValue(id)->load();
+    };
 
-    p.hfCornerHz = v("HF_CORNER");
+    p.eqColor = value("EQ_COLOR");
+    p.hfCornerHz = value("HF_CORNER");
 
-    p.ottDepth = v("OTT_DEPTH");
-    p.ottMix = v("OTT_MIX");
-    p.ottThresholdDb = v("OTT_THRESHOLD");
-    p.ottUpRatio = v("OTT_UP_RATIO");
-    p.ottDownRatio = v("OTT_DOWN_RATIO");
-    p.ottAttackMs = v("OTT_ATTACK");
-    p.ottReleaseMs = v("OTT_RELEASE");
-    p.ottLowMidHz = v("OTT_X1");
-    p.ottMidHighHz = v("OTT_X2");
-    p.ottHighMidHz = v("OTT_X3");
-    p.ottPostGainDb = v("OTT_POST");
+    p.ottMix = value("OTT_MIX");
+    p.ottThreshold = value("OTT_THRESHOLD");
+    p.ottUpRatio = value("OTT_UP_RATIO");
+    p.ottDownRatio = value("OTT_DOWN_RATIO");
+    p.ottAttackMs = value("OTT_ATTACK");
+    p.ottReleaseMs = value("OTT_RELEASE");
+    p.ottX1 = value("OTT_X1");
+    p.ottX2 = value("OTT_X2");
+    p.ottX3 = value("OTT_X3");
+    p.ottInputGainDb = value("OTT_INPUT");
+    p.ottPostGainDb = value("OTT_POST");
 
-    p.atypeAmount = v("ATYPE_AMOUNT");
-    p.atypeDriveDb = v("ATYPE_DRIVE");
-    p.atypeBias = v("ATYPE_BIAS");
-    p.atypeMix = v("ATYPE_MIX");
-    p.atypeTone = v("ATYPE_TONE");
-    p.atypeHpfHz = v("ATYPE_HPF");
+    p.atypeAttackMs = value("ATYPE_ATTACK");
+    p.atypeReleaseMs = value("ATYPE_RELEASE");
+    p.atypeMix = value("ATYPE_MIX");
 
-    p.deessFreq = v("DEESS_FREQ");
-    p.deessQ = v("DEESS_Q");
-    p.deessThreshold = v("DEESS_THRESHOLD");
-    p.deessRange = v("DEESS_RANGE");
-    p.deessAttackMs = v("DEESS_ATTACK");
-    p.deessReleaseMs = v("DEESS_RELEASE");
+    p.deessLowHz = value("DEESS_LOW");
+    p.deessHighHz = value("DEESS_HIGH");
+    p.deessRangeDb = value("DEESS_RANGE");
+    p.deessStrength = value("DEESS_STRENGTH");
+    p.deessAttackMs = value("DEESS_ATTACK");
+    p.deessReleaseMs = value("DEESS_RELEASE");
     p.deessListen = apvts.getRawParameterValue("DEESS_LISTEN")->load() > 0.5f;
 
-    p.dryWet = v("DRY_WET");
-    p.outputDb = v("OUTPUT_LEVEL");
+    p.dryWet = value("DRY_WET");
+    p.outputDb = value("OUTPUT_LEVEL");
 
     dsp.process(buffer, p);
 }
