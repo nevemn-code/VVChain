c5b417f37d865a119ac38c69003b454c66e32845
nevemn-code <nevemann@gmail.com>
2026-09-20 23:33:09 +0800

    Use exact dynamic EQ source marker in stress suite

--- Tests/reference_stress.py
+++ Tests/reference_stress.py
@@ -219,7 +219,7 @@ def structure_checks():
 
     for token in [
         "DEESS_VOICE", "DEESS_INTENSITY", "DEESS_OFFSET",
-        "OTT_DEGREE", "ATYPE_DEGREE", "EQ" + " + n + " + "_FREQ",
+        "OTT_DEGREE", "ATYPE_DEGREE", '"EQ" + n + "_FREQ"',
     ]:
         assert token in pp, token
 
