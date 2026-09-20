65a3775de9538d0a36bca6706a13c5c08830e6a1
nevemn-code <nevemann@gmail.com>
2026-09-20 22:47:54 +0800

    Expand native editor control bank for module detail panels

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -24,12 +24,13 @@ private:
     VVChainAudioProcessor& audioProcessor;
     int moduleIndex = 0;
     int bandIndex = 0;
+    bool detailOpen = false;
 
     std::array<juce::TextButton, 6> moduleButtons;
     std::array<juce::TextButton, 4> bandButtons;
-    std::array<juce::Slider, 12> sliders;
-    std::array<juce::Label, 12> labels;
-    std::array<std::unique_ptr<Attachment>, 12> attachments;
+    std::array<juce::Slider, 16> sliders;
+    std::array<juce::Label, 16> labels;
+    std::array<std::unique_ptr<Attachment>, 16> attachments;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VVChainAudioProcessorEditor)
 };
