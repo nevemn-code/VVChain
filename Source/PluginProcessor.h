803d54f389a10dcb2480a233d7859ad6d46f6b47
nevemn-code <nevemann@gmail.com>
2026-09-20 22:20:26 +0800

    VVChain: add Source/PluginProcessor.h

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "DSP/ChainDSP.h"
+
+class VVChainAudioProcessor final : public juce::AudioProcessor
+{
+public:
+    VVChainAudioProcessor();
+    ~VVChainAudioProcessor() override = default;
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override {}
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return "VVChain"; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
+
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram(int) override {}
+    const juce::String getProgramName(int) override { return {}; }
+    void changeProgramName(int, const juce::String&) override {}
+
+    void getStateInformation(juce::MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+private:
+    VVChainDSP dsp;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VVChainAudioProcessor)
+};
