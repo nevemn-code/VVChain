d071f3f4beec9b95de6f0a3df4f16824477a22cc
nevemn-code <nevemann@gmail.com>
2026-09-20 22:41:27 +0800

    Make native editor controls dynamic

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,8 +14,22 @@ public:
     void resized() override;
 
 private:
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
     void timerCallback() override;
+    void selectModule(int index);
+    void selectBand(int index);
+    void rebuildControls();
+
     VVChainAudioProcessor& audioProcessor;
-    std::array<juce::Point<float>, 4> bandPoints;
+    int moduleIndex = 0;
+    int bandIndex = 0;
+
+    std::array<juce::TextButton, 6> moduleButtons;
+    std::array<juce::TextButton, 4> bandButtons;
+    std::array<juce::Slider, 12> sliders;
+    std::array<juce::Label, 12> labels;
+    std::array<std::unique_ptr<Attachment>, 12> attachments;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VVChainAudioProcessorEditor)
 };
