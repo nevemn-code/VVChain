622ba8e01c10616668f47836c8b699c0e563b02e
nevemn-code <nevemann@gmail.com>
2026-09-20 23:28:52 +0800

    Rewrite plugin UI with plugin-style controls and real analyzer views

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <array>
+#include <memory>
 #include "PluginProcessor.h"
 
 class VVChainAudioProcessorEditor final : public juce::AudioProcessorEditor,
@@ -14,42 +16,77 @@ public:
     void resized() override;
     void mouseDown(const juce::MouseEvent&) override;
     void mouseDrag(const juce::MouseEvent&) override;
+    void mouseUp(const juce::MouseEvent&) override;
 
 private:
     using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    using ComboAttachment = juce::AudioProcessorValueTreeState::ComboBoxAttachment;
 
-    enum DragTarget
+    enum class DragTarget
     {
-        DragNone = 0,
+        None,
         EqBand1, EqBand2, EqBand3, EqBand4,
         OttDegree1, OttDegree2, OttDegree3, OttDegree4,
-        OttXover1, OttXover2, OttXover3,
+        OttX1, OttX2, OttX3,
         TypeDegree1, TypeDegree2, TypeDegree3, TypeDegree4,
-        DeessLow, DeessHigh, DeessStrength, DeessRange,
+        DeEssIntensity, DeEssOffset,
         MixDryWet, MixOutput
     };
 
-    void timerCallback() override;
     void selectModule(int index);
     void selectBand(int index);
     void rebuildControls();
+
+    void hideControl(int index);
+    void setControl(int index, const juce::String& parameterId, const juce::String& title);
+    void setControlRangeForDisplay(int index, double minimum, double maximum, double step, const juce::String& suffix);
+
+    void drawTopBar(juce::Graphics&, juce::Rectangle<float>);
+    void drawGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawParameterPanel(juce::Graphics&, juce::Rectangle<float>);
+
+    void drawGrid(juce::Graphics&, juce::Rectangle<float>, float minDb, float maxDb);
+    void drawEqGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawOttGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawTypeAGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawDeEsserGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawAnalyzerGraph(juce::Graphics&, juce::Rectangle<float>);
+    void drawMixGraph(juce::Graphics&, juce::Rectangle<float>);
 
     float graphFrequencyToX(const juce::Rectangle<float>&, float hz) const;
     float graphXToFrequency(const juce::Rectangle<float>&, float x) const;
     float graphPercentToY(const juce::Rectangle<float>&, float percent) const;
     float graphYToPercent(const juce::Rectangle<float>&, float y) const;
 
+    float parameterValue(const juce::String& id) const;
+    void setParameter(const juce::String& id, float value);
+
+    void timerCallback() override;
+
     VVChainAudioProcessor& audioProcessor;
     int moduleIndex = 0;
     int bandIndex = 0;
-    bool detailOpen = true;
-    int dragTarget = DragNone;
+    DragTarget dragTarget = DragTarget::None;
 
     std::array<juce::TextButton, 6> moduleButtons;
     std::array<juce::TextButton, 4> bandButtons;
-    std::array<juce::Slider, 20> sliders;
-    std::array<juce::Label, 20> labels;
-    std::array<std::unique_ptr<Attachment>, 20> attachments;
+    std::array<juce::Slider, 18> controls;
+    std::array<juce::Label, 18> controlLabels;
+    std::array<std::unique_ptr<Attachment>, 18> attachments;
+
+    juce::ComboBox deEssVoice;
+    std::unique_ptr<ComboAttachment> deEssVoiceAttachment;
+
+    juce::ToggleButton ottClipper { "CLIP" };
+    juce::ToggleButton analyzerAverage { "Average" };
+    juce::ToggleButton analyzerPeak { "Max Hold" };
+    juce::ToggleButton analyzerPersistence { "Persistence" };
+    juce::ToggleButton analyzerSmooth { "Smoothing" };
+
+    std::array<float, VVChainAudioProcessor::kSpectrumBins> spectrum {};
+    std::array<float, VVChainAudioProcessor::kSpectrumBins> peakSpectrum {};
+    std::array<float, 256> analyzerSmoothed {};
+    std::array<std::array<float, 256>, 48> waterfall {};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VVChainAudioProcessorEditor)
 };
