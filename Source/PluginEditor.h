d6dada6235d73145a64651184b272dc38b3dfed8
nevemn-code <nevemann@gmail.com>
2026-09-21 13:48:55 +0800

    Add top-level Analog Color bypass module slot

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -88,8 +88,8 @@ private:
     MetalLookAndFeel metalLook;
 
     std::vector<Knob> knobs;
-    std::array<std::unique_ptr<juce::ToggleButton>, 4> bypassButtons;
-    std::array<std::unique_ptr<BoolAttachment>, 4> bypassAttachments;
+    std::array<std::unique_ptr<juce::ToggleButton>, 5> bypassButtons;
+    std::array<std::unique_ptr<BoolAttachment>, 5> bypassAttachments;
     std::unique_ptr<juce::ToggleButton> masterBypassButton;
     std::unique_ptr<BoolAttachment> masterBypassAttachment;
     std::unique_ptr<juce::ToggleButton> deessBypassButton;
