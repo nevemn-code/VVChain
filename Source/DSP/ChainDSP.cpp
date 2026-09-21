0159062f0f86bc95b69f63fc7425ace358e2ee29
nevemn-code <nevemann@gmail.com>
2026-09-21 14:06:40 +0800

    Define shared state-preserving crossover coefficient updater

--- Source/DSP/ChainDSP.cpp
+++ Source/DSP/ChainDSP.cpp
@@ -74,6 +74,14 @@ void VVChainDSP::updateHighPass(Biquad& filter, double fs, double f0, double q)
     updateAnalogHighPass(filter, fs, f0, q);
 }
 
+void VVChainDSP::updateCrossover(Crossover4th& xover, double fs, double f0, double q)
+{
+    updateLowPass(xover.lp1, fs, f0, q);
+    updateLowPass(xover.lp2, fs, f0, q);
+    updateHighPass(xover.hp1, fs, f0, q);
+    updateHighPass(xover.hp2, fs, f0, q);
+}
+
 float VVChainDSP::dbToGain(float db) noexcept
 {
     return juce::Decibels::decibelsToGain(db);
