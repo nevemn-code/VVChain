bdeee560801a49171c181658eebd8479d6c119ea
nevemn-code <nevemann@gmail.com>
2026-09-24 10:02:52 +0800

    [v1.0.10] Bump Web DSP version

--- docs/vvchain-worklet.js
+++ docs/vvchain-worklet.js
@@ -1,4 +1,4 @@
-// VVChain Web AudioWorklet DSP module · v1.0.9
+// VVChain Web AudioWorklet DSP module · v1.0.10
 class VVChainWorklet extends AudioWorkletProcessor {
   constructor(){
     super();
