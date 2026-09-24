eeca7491d7884902ee37a1e1df6f07be3e0e1189
nevemn-code <nevemann@gmail.com>
2026-09-24 10:41:50 +0800

    [v1.0.13] Synchronize project version

--- docs/vvchain-worklet.js
+++ docs/vvchain-worklet.js
@@ -1,4 +1,4 @@
-// VVChain Web AudioWorklet DSP module · v1.0.12
+// VVChain Web AudioWorklet DSP module · v1.0.13
 class VVChainWorklet extends AudioWorkletProcessor {
   constructor(){
     super();
